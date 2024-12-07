9c42c2a9eb0a5a87d84d57925ad873098d09dedb
LucasEBotelho <lucasone2050@hotmail.com>
2024-12-07 02:04:24 -0300

    Alteracao na fonte e na estrutura da page

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,13 +1,35 @@
-import Header from "./components/header";
-import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import React, { useState } from 'react';
 
-function App() {
+function ListarTarefa() {
+  const [tarefas, setTarefas] = useState(['Tarefa 1', 'Tarefa 2', 'Tarefa 3']);
+  const [novaTarefa, setNovaTarefa] = useState('');
+
+  const adicionarTarefa = () => {
+    if (novaTarefa) {
+      setTarefas([...tarefas, novaTarefa]);
+      setNovaTarefa(''); // Limpa o campo de input
+    }
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <ListarTarefa />
+    <div>
+      <h2>Lista de Tarefas</h2>
+      <ul>
+        {tarefas.map((tarefa, index) => (
+          <li key={index}>{tarefa}</li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={novaTarefa}
+          onChange={(e) => setNovaTarefa(e.target.value)}
+          placeholder="Digite uma nova tarefa"
+        />
+        <button onClick={adicionarTarefa}>Adicionar Tarefa</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default ListarTarefa;
